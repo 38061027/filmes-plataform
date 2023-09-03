2b8e2f522ee325052b7e3d0d02e8be4e64df47ad
José Augusto <joseaugustogomessantos12@gmail.com>
2023-09-03 12:40:12 -0300

    Criando o formControls e validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { RodapeComponent } from './shared/components/rodape/rodape.component';
 import { TabelaComponent } from './shared/components/tabela/tabela.component';
 import { TopoComponent } from './shared/components/topo/topo.component';
 import { MaterialModule } from './shared/material/material.module';
-
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,9 +25,10 @@ import { MaterialModule } from './shared/material/material.module';
     AppRoutingModule,
     FilmesModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/filmes/filmes.module.ts
+++ src/app/filmes/filmes.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { CadastroFilmesComponent } from './cadastro-filmes/cadastro-filmes.component';
 import { ListagemFilmesComponent } from './listagem-filmes/listagem-filmes.component';
 import { MaterialModule } from '../shared/material/material.module';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,9 @@ import { MaterialModule } from '../shared/material/material.module';
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class FilmesModule { }
